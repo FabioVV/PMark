e29bd40a2a75913280621a7b44dbcc39c7adb32d
FabioVV <fabio-varela@protonmail.com>
2025-10-20 15:59:35 -0300

    Initial commit #2

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# vssg
-Varela's static site generator. My very own static site generator for my stuff
+# VSSG
+
+## Varela's static site generator.
+
+This is a static site generator written in Python meant to be fast and used with minimal setup.
+I use it to generate my stuff, but if you find it useful, feel free to contribute to the project and/or use it for your stuff too.
+
+## Requirements
+
+- Python 3.6+
+
+## Usage
---
+++ run.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import os
+import sys
+import subprocess
+
+target = os.path.join(os.getcwd(), "src", "main.py")
+
+
+if os.path.exists(target):
+    try:
+        args_cmd = sys.argv[1:]
+
+        if len(sys.argv) <= 1:
+            args_cmd = []
+
+        result = subprocess.run([sys.executable, target] + args_cmd)
+        if result.returncode != 0:
+            print("Error: subprocess returned non-zero exit code")
+            print(result)
+            sys.exit(result.returncode)
+
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
