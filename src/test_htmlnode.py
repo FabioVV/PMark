50f3fd870253d6684170b8accb92fa1d9b447bf6
FabioVV <fabio-varela@protonmail.com>
2025-10-20 17:01:08 -0300

    add missing htmlnode test

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -12,6 +12,13 @@ class TestHtmlNode(unittest.TestCase):
         self.assertIsNone(nd2.attrs)
         self.assertIsNone(nd3.attrs)
 
+    def test_attrs_is_not_none(self):
+        nd1 = HTMLNode(
+            "h1", "Hello, World!", None, {"class": "header1", "id": "main-header"}
+        )
+
+        self.assertIsNotNone(nd1.attrs)
+
     def test_format_attrs(self):
         nd1 = HTMLNode(
             "h1", "Hello, World!", None, {"class": "header1", "id": "main-header"}
