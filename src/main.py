610d04c9a2fc696fbca16ff541a0b55e7461bbb6
FabioVV <fabio-varela@protonmail.com>
2025-10-21 13:24:24 -0300

    Rename utils, add a new fn split_nodes_delimiter to help convert from markdown to HTML

--- src/main.py
+++ src/main.py
@@ -1,23 +1,8 @@
 from textnode import TextNode, TextType
 from htmlnode import ParentNode, LeafNode, HTMLNode
+from textnode_utils import split_nodes_delimiter
 
-nd1 = ParentNode(
-    "h1",
-    [
-        LeafNode("b", "Hello, World!"),
-        LeafNode(None, "Just some text"),
-        LeafNode("span", "Welcome!"),
-    ],
-    None,
-)
+node = TextNode("This is text with a _code block_ word", TextType.PLAIN_TEXT)
+new_nodes = split_nodes_delimiter([node], "_", TextType.UNDERLINE_TEXT)
 
-nd2 = LeafNode("h3", "This is it.", {"class": "important"})
-
-nd3 = ParentNode(
-    "article",
-    [nd1, nd2],
-    {"style": "background-color:yellow; color:red;"},
-)
-
-with open("out.html", "w", encoding="utf-8") as f:
-    _ = f.write(nd3.to_html())
+print(new_nodes)
---
+++ src/textnode_utils.py
@@ -0,0 +1,69 @@
+from textnode import TextNode, TextType
+from htmlnode import LeafNode
+
+
+def make_text_node(text: str, text_type: TextType, url: str | None = None) -> TextNode:
+    return TextNode(text, text_type, url)
+
+
+def text_node_to_html_node(txt_node: TextNode) -> LeafNode:
+    match txt_node.text_type:
+        case TextType.PLAIN_TEXT:
+            return LeafNode(None, txt_node.text)
+        case TextType.BOLD_TEXT:
+            return LeafNode("b", txt_node.text)
+        case TextType.ITALIC_TEXT:
+            return LeafNode("i", txt_node.text)
+        case TextType.UNDERLINE_TEXT:
+            return LeafNode("u", txt_node.text)
+        case TextType.STRIKETHROUGH_TEXT:
+            return LeafNode("s", txt_node.text)
+        case TextType.CODE_TEXT:
+            return LeafNode("code", txt_node.text)
+        case TextType.LINK_TEXT:
+            return LeafNode("a", txt_node.text, {"href": txt_node.url or "#"})
+        case TextType.IMAGE_TEXT:
+            return LeafNode(
+                "img", "", {"src": txt_node.url or "", "alt": txt_node.text}
+            )
+        case _:
+            raise ValueError(f"Unexpected TextType: {txt_node.text_type}")
+
+
+def split_nodes_delimiter(
+    old_nodes: list[TextNode], delimiter: str, text_type: TextType
+) -> list[TextNode]:
+    new_nodes: list[TextNode] = []
+
+    for i in old_nodes:
+        if i.text_type != TextType.PLAIN_TEXT:
+            new_nodes.append(i)
+            continue
+
+        text_node: TextNode | None = None
+        splitted_str = i.text.split(delimiter)
+        if len(splitted_str) == 1:
+            raise ValueError("Delimiter not found in text")
+
+        start_del: int = i.text.find(delimiter)
+        if (start_del) == -1:
+            raise ValueError("Delimiter not found in text")
+
+        end_del: int = i.text.find(delimiter, start_del + len(delimiter))
+        if (end_del) == -1:
+            raise ValueError("Closing delimiter not found in text")
+
+        text_to_format = i.text[start_del + len(delimiter) : end_del]
+
+        if delimiter == "`" or delimiter == "_" or delimiter == "**":
+            text_node = make_text_node(text_to_format, text_type)
+        else:
+            raise ValueError("Delimiter not supported")
+
+        for x in splitted_str:
+            if x == text_to_format:
+                new_nodes.append(text_node)
+            else:
+                new_nodes.append(make_text_node(x, TextType.PLAIN_TEXT))
+
+    return new_nodes
