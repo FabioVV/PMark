512b52c3965389657e16df4e9f184536d7ca9fe3
FabioVV <fabio-varela@protonmail.com>
2025-10-24 13:43:54 -0300

    Start work on generation, add ideas.txt

---
+++ src/gen.py
@@ -0,0 +1,5 @@
+def generate_page_from_path(
+    src: str, dst: str = "public", template: str = "template.html"
+):
+    """Generates a single HTML page from a given markdown file using a base template"""
+    ...
